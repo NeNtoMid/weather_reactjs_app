6158ccc5ccf188555c6c05fe5795b1ed16b679da
= <NeNtoMid0@gmail.com>
2021-01-11 19:10:36 +0100

    Set weatherIcon width and height

--- src/components/UI/WeatherIcon/WeatherIcon.js
+++ src/components/UI/WeatherIcon/WeatherIcon.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ClimateIcon = ({ weatherIcon }) => {
-	return <img src={`https:${weatherIcon}`} alt='weather icon' />;
+	return (
+		<img
+			src={`https:${weatherIcon}`}
+			style={{ width: 64, height: 64 }}
+			alt='weather icon'
+		/>
+	);
 };
 
 ClimateIcon.propTypes = {
